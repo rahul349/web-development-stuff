ed2f42516e58113ca38230a0104fbc9ee4de07fa
rahul349 <56111097+rahul349@users.noreply.github.com>
2020-06-07 19:34:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.postwrapper').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+});
+    
+
